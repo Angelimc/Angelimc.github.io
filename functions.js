c75586989b4832f4f0c525a336f7a0e352fe34dc
Angelimc <a.corpin@alumni.ubc.ca>
2018-05-25 10:40:55 -0700

    added all webpages except projects page

--- functions.js
+++ functions.js
@@ -1,8 +1,6 @@
 function ShowContent(content) {
   document.getElementById("home").style.display = 'none';
   document.getElementById("projects").style.display = 'none';
-  document.getElementById("linkedIn").style.display = 'none';
   document.getElementById("resume").style.display = 'none';
-  document.getElementById("email").style.display = 'none';
   document.getElementById(content).style.display = 'block';
 }
